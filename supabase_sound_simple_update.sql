3a7e8be24791bbe139e1d378eb773008f7235662
raphaellim <138130040+raphaellim@users.noreply.github.com>
2026-06-06 00:49:46 +0900

    Delete supabase_sound_simple_update.sql

--- supabase_sound_simple_update.sql
+++
@@ -1,3 +0,0 @@
--- 수동 사운드 선택 필드 추가
-alter table donations add column if not exists manual_sound_file text default '';
-alter table donations add column if not exists manual_sound_title text default '';
